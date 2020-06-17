b5aaf658e751b4473f86a9299943b664ed09fb0b
Caropox <54894433+Caropox@users.noreply.github.com>
2020-06-17 10:47:58 +0200

    code en commantaire pour des tests

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+/** 
 Barba.Pjax.start();
 
 var FadeTransition = Barba.BaseTransition.extend({
@@ -8,45 +9,23 @@ var FadeTransition = Barba.BaseTransition.extend({
        * (Barba.js also comes with an handy Promise polyfill!)
        */
   
-      // As soon the loading is finished and the old page is faded out, let's fade the new page
+      /* As soon the loading is finished and the old page is faded out, let's fade the new page
       Promise
         .all([this.newContainerLoading, this.fadeOut()])
         .then(this.fadeIn.bind(this));
     },
   
-    fadeOut: function() {
-      /**
-       * this.oldContainer is the HTMLElement of the old Container
-       */
-  
-      return $(this.oldContainer).animate({ opacity: 0 }).promise();
-    },
-  
+    fadeOut: function() {},
+    
     fadeIn: function() {
-      /**
-       * this.newContainer is the HTMLElement of the new Container
-       * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-       * Please note, newContainer is available just after newContainerLoading is resolved!
-       */
-  
-      var _this = this;
-      var $el = $(this.newContainer);
-  
-      $(this.oldContainer).hide();
-  
-      $el.css({
-        visibility : 'visible',
-        opacity : 0
-      });
-  
-      $el.animate({ opacity: 1 }, 400, function() {
-        /**
-         * Do not forget to call .done() as soon your transition is finished!
-         * .done() will automatically remove from the DOM the old Container
-         */
-  
-        _this.done();
-      });
+        this.newContainer.classList.add("slide-in");
+
+        var that = this;
+
+        this.newContainer.addEventListener('animationend', function(){
+        this.newContainer.classList.remove("slide-in");
+        that.done();
+        });
     }
   });
   
@@ -54,11 +33,13 @@ var FadeTransition = Barba.BaseTransition.extend({
    * Next step, you have to tell Barba to use the new Transition
    */
   
+   /*
   Barba.Pjax.getTransition = function() {
     /**
      * Here you can use your own logic!
      * For example you can use different Transition based on the current page or link...
      */
   
+     /*
     return FadeTransition;
-  };
+  };*/
